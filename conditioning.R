575f9a608287f5aa62efa192eca5d8bde5cde826
tberkery <78045727+tberkery@users.noreply.github.com>
2022-12-04 11:22:40 -0500

    Continuing data conditioning

--- conditioning.R
+++ conditioning.R
@@ -1,19 +1,101 @@
 library(tidyverse)
+library(lubridate)
 
-plays = read_csv("plays.csv")
-plays_passing = plays %>% 
-  filter(passResult != 'R') %>%
-  filter(passResult != ' ')
+week_num = 1
 
-plays_passing = plays_passing %>%
-  filter(!(foulName1 %in% c('Offensive Holding', 'Defensive Offside', 'Illegal Formation',
+run = function() {
+  weeks = load_weeks()
+  plays = load_plays()
+}
+
+read_plays = function() {
+  plays = read_csv("plays.csv")
+  return(plays)
+}
+
+read_week = function(week_num) {
+  week = read_csv(paste0("week", week_num, ".csv"))
+  return(week)
+}
+
+
+load_plays = function() {
+  plays = read_plays()
+  plays_conditioned = condition_plays(plays)
+  return(plays_conditioned)
+}
+
+load_weeks = function() {
+  weeks = read_week(1)
+  weeks = condition_week_data(weeks)
+  # for (i in 2:8) {
+  #   new_week = read_week(i)
+  #   new_week_conditioned = condition_week_data(new_week)
+  #   weeks = rbind(weeks, new_week_conditioned)
+  # }
+  return(weeks)
+}
+
+condition_plays = function(plays){
+  plays_passing = plays %>% 
+    filter(passResult != 'R') %>%
+    filter(passResult != ' ')
+  plays_passing = plays_passing %>%
+    filter(!(foulName1 %in% c('Offensive Holding', 'Defensive Offside', 'Illegal Formation',
                               'Illegal Formation', 'Illegal Block Above the Waist', 'Face Mask (15 Yards)',
                               'Disqualification', 'Illegal Blindside Block', 'Chop Block',
                               'Low Block', 'Illegal Shift', 'Tripping', 'Illegal Use of Hands',
                               'Illegal Substitution', 'Illegal Motion', 'Clipping')))
+  return(plays_passing)
+}
 
-week1 = read_csv("week1.csv")
+condition_week_data = function(week) {
+  
+  week_data = week %>%
+    mutate(is_throw = ifelse(str_detect(event, "pass") & !str_detect(event, 'outcome'), TRUE, FALSE)) %>%
+    mutate(time_of_throw = ifelse(str_detect(event, "pass") & !str_detect(event, 'outcome'), time, NA)) %>%
+    arrange(gameId, playId, nflId, time, event, is_throw) %>%
+    group_by(gameId, playId, nflId, time) %>%
+    mutate(across(c(x, y, s, a, o), ~mean(., na.rm = TRUE))) %>%
+    arrange(gameId, playId, nflId, time, event) %>%
+    distinct(gameId, playId, nflId, time, .keep_all = TRUE) %>%
+    ungroup()
+    
+  
+  week_data = week_data %>%
+    ungroup() %>%
+    mutate(play_id_prev = lag(playId),
+           game_id_prev = lag(gameId),
+           nfl_id_prev = lag(nflId)) %>%
+    mutate(same_play_as_prev = ifelse(play_id_prev == playId & gameId == game_id_prev & nflId == nfl_id_prev, TRUE, FALSE)) %>%
+    mutate(play_time_index = 0) %>%
+    mutate(play_time_index = ifelse(same_play_as_prev, lag(play_time_index) + 0.1, 0))
+  
+  play_starts = week_data %>%
+    mutate(is_snap = ifelse(str_detect(event, 'snap'), TRUE, FALSE)) %>%
+    filter(is_snap == TRUE) %>%
+    group_by(gameId, playId, nflId) %>%
+    mutate(time = min(time, na.rm = TRUE)) %>%
+    ungroup() %>%
+    select(gameId, playId, nflId, event, time) %>%
+    distinct(gameId, playId, nflId, time, .keep_all = TRUE)
+  
+  week_data_with_play_start = week_data %>%
+    left_join(play_starts, by = c('gameId', 'playId', 'nflId'), suffix = c('', '_play_start')) %>%
+    mutate(time_since_play_start = time - time_play_start)
+  return(week_data_with_play_start)
+}
 
-week1_subset = week1 %>% 
-  filter(event %in% c('ball_snap', 'autoevent_ballsnap', 'autoevent_passforward', 'pass_forward', 'qb_sack', 'qb_strip_sack'))
-
+create_weekly_play_summary = function(weeks) {
+  weeks_summarized = weeks %>%
+    mutate(time_since_play_start_rounded = round(time_since_play_start, 3)) %>%
+    filter(!is.na(time_since_play_start)) %>%
+    filter(time_since_play_start >= 0) %>%
+    filter(time_since_play_start_rounded < 1) %>%
+    arrange(time_since_play_start_rounded)
+  weeks_summarized = weeks_summarized %>%
+    pivot_wider(id_cols = c(gameId, playId, nflId),
+                names_from = time_since_play_start_rounded,
+                values_from = c(x, y, s, a, dis, o, dir))
+  write_csv(weeks_summarized, "plays_by_player_with_motion.csv")
+}
